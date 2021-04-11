ac79675e817686696f6f96b17d568c1aee3deda5
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-04-11 13:16:54 -0300

    :lipstick: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div >
      <h1>hello</h1>
+      <GlobalStyle/>
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --purple: #5429CC;
+    --purple-light:#6933FF;
+    --green: #33CC95;
+    --green-shadow: #12A454;
+    --red: #E62E4D;
+    
+    --text-title:#363F5F;
+    --text-body:#969CB3;
+    
+    --background:#F0F2F5;
+    --shape:#FFF;
+  }
+
+  * {
+    margin: 0; 
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;//15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;//14px
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+  h1,h2,h3,h4,h5,h6,strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disable] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
