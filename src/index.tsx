891a80c4e50a7880dce32f613d1c51fa11c8ee40
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-05-06 09:37:10 -0300

    using mirage schema Model to save data

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer , Model } from 'miragejs';
 
 import {App} from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  }, 
+
   routes(){
     this.namespace = 'api';
 
     this.get("/transactions", () => {
-      return [{
-        id: 1,
-        title: 'Transaction 1',
-				amount: 400,
-				type: 'deposit',
-      }]
+      return this.schema.all('transaction')
     });
 
     this.post('/transactions', (schema, request) => {
       //access data in the BODY request and parse JSON to javascript
       const data = JSON.parse(request.requestBody);
-      return data;
+      return schema.create('transaction', data)
     });
 
   }
