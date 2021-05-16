3c159d8134261df9e57910f3d4dffaf4d20f7e8a
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-05-16 15:45:59 -0300

    change trasaction context to provider the context as a component TransactionProvider

---
+++ src/TransactionContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState, ReactNode } from 'react';
+import { api } from "./services/api";
+
+export const TransactionContext = createContext<Transaction[]>([]);
+
+interface Transaction{
+  id: number,
+  title: string,
+  amount: number,
+  type: string,
+  category: string,
+  createdAt: string,
+}
+
+interface TransactionProviderProps{
+  children: ReactNode;
+}
+
+export function TransactionProvider({children}: TransactionProviderProps){
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(()=>{
+    api.get('transactions')
+    .then(response => setTransactions(response.data.transactions))
+  },[])
+
+  return <TransactionContext.Provider value={transactions}>{children}</TransactionContext.Provider>
+}
